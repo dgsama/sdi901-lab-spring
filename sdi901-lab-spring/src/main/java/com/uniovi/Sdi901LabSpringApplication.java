325bc118ff1eecae77046d54b3a113500ab3ce7f
dgsama <dgsama141@gmail.com>
2019-02-18 12:21:11 +0100

    Creación inicial del proyecto Notaneitor

---
+++ sdi901-lab-spring/src/main/java/com/uniovi/Sdi901LabSpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi901LabSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi901LabSpringApplication.class, args);
+	}
+
+}
